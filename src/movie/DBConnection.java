ecc134c536011b875a3a4691338c082505277cff
Saravana Kumar G.M <saravanakumargm00@gmail.com>
2024-08-23 11:27:41 +0530

    Update

--- src/movie/DBConnection.java
+++ src/movie/DBConnection.java
@@ -6,6 +6,6 @@ import java.sql.SQLException;
 
 public class DBConnection {
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/moviedb","root","root");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/moviedb","","");
     }
 }
